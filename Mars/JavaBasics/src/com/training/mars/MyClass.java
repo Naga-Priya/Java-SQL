34476dc93494d8b2b73dda1e9c34592e1070cd50
naga-priya <nagapriya.cks@gmail.com>
2022-04-11 09:08:37 -0700

    Commit using Eclipse IDE

--- Mars/JavaBasics/src/com/training/mars/MyClass.java
+++ Mars/JavaBasics/src/com/training/mars/MyClass.java
@@ -7,3 +7,28 @@ public class MyClass {
 		//sysout
 	}
 }
+/*Concepts:
+Access Specifiers - public, private, protected
+
+static: In java we need an object to access a class methods.
+when we declare a method as static, we can call it without an object.
+main method is static. Jvm looks for main method to start an application.
+
+primitive vs non-primitive default values
+
+variable - named memory location that contains data
+types of variables: local / instance/ static
+local - available only inside a block
+instance- available for that object
+instance - shared across objects and is accessed without instance
+
+memory management - garbage collector finds unused variables and free up memory space
+
+reference variables are treated as pointers in java
+
+heap - objects reside in heap
+stack - primitives and reference types reside here
+
+varaible name:starts with char or (_ or $), camelCase,
+
+*/
