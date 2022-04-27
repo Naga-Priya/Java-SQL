8fbb123b69511e0b51f4413d1228e38ef95bd914
naga-priya <nagapriya.cks@gmail.com>
2022-04-27 01:52:46 -0700

    Java based configuration for Spring MVC

--- Spring-mvc/src/main/java/com/training/springmvc/controller/HomeController.java
+++ Spring-mvc/src/main/java/com/training/springmvc/controller/HomeController.java
@@ -2,7 +2,11 @@ package com.training.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
@@ -18,5 +22,30 @@ public class HomeController {
 		model.addAttribute("value","New message from Spring MVC!");
 		return "message";
 	}
+	@RequestMapping("/")
+	public String displayLogin(ModelMap model) {
+		model.addAttribute("message","Hello World from Spring MVC!");
+		return "login";
+	}
 	
+	//@RequestMapping("/validate?uname={name}&pwd={pwd}",method=RequestMethod.GET)
+	@GetMapping("/validate")
+//	public String validate(@RequestBody String fullName,ModelMap model)
+	//public String validate(@PathVariable String uname,@PathVariable String pwd,ModelMap model)
+	public String validate(@RequestParam String uname,
+			@RequestParam String pwd,
+			ModelMap model)
+	{
+		//String name = (String) request.getAttribute("uname");
+		
+		//model.addAttribute("name",uname+"-"+pwd);
+	//model.addAttribute("name",fullName);
+		if(uname.equalsIgnoreCase("priya")&&pwd.equals("password")) {
+			model.addAttribute("name",uname);
+			return "welcome";
+		}
+		else
+			return "error";
+	
+	}
 }
--- Spring-mvc/src/main/webapp/WEB-INF/pages/login.jsp
+++ Spring-mvc/src/main/webapp/WEB-INF/pages/login.jsp
@@ -7,6 +7,12 @@
 <title>Insert title here</title>
 </head>
 <body>
-
+<h1>Mars Returnship</h1>
+<h2>Login</h2>
+<form action = "<%=request.getContextPath()%>/validate" method = "get">
+<input type = "text" name = "uname" placeholder = "user name"/> <BR><br>
+<input type = "password" name = "pwd" placeholder = "password"/> 
+<input type = "submit"  value = "login here"/>
+</form>
 </body>
 </html>
